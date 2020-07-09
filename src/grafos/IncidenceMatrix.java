e24301a478a8c3c835e46944519e0f25520b818c
user <IronDev>
2020-07-09 16:40:37 -0500

    Distancia mínima

--- src/grafos/IncidenceMatrix.java
+++ src/grafos/IncidenceMatrix.java
@@ -23,4 +23,21 @@ public class IncidenceMatrix {
         return false;
     }
     
+    public void print(){
+        System.out.println("");
+        System.out.print("  |");
+        for (int i = 0; i < matrix[0].length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < matrix.length; i++){
+            System.out.print(i + " |");
+            for (int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+    
 }
